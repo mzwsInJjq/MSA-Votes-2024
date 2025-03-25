9f59801bd4e603a58ebf661b3e58cd3edc078a5f
J. Chen <140699748+mzwsInJjq@users.noreply.github.com>
2025-03-25 23:11:34 +0800

    Add files via upload

--- New_York/nyc_msa_election_2024_coastline.py
+++ New_York/nyc_msa_election_2024_coastline.py
@@ -9,6 +9,7 @@ from matplotlib.colors import TwoSlopeNorm
 # Load the data
 gdf1 = gpd.read_file(r'C:\Users\Chen\Downloads\NY-precincts-with-results.geojson')
 gdf2 = gpd.read_file(r'C:\Users\Chen\Downloads\NJ24\2024 NJ Precincts.shp')
+gdf3 = gpd.read_file(r'C:\Users\Chen\Downloads\NJ-precincts-with-results.geojson')
 
 df = gpd.read_file(r"C:\Users\Chen\Downloads\nhgis0001_shapefile_tl2023_us_state_2023\US_state_2023.shp")
 df = df[(df['STATEFP'] == '36') | (df['STATEFP'] == '34')]
@@ -18,13 +19,16 @@ df = df.to_crs("epsg:4326")
 gdf1 = gdf1[gdf1['GEOID'].str.startswith('36047') | gdf1['GEOID'].str.startswith('36081') | gdf1['GEOID'].str.startswith('36061') | gdf1['GEOID'].str.startswith('36005') | gdf1['GEOID'].str.startswith('36119') | gdf1['GEOID'].str.startswith('36085') | gdf1['GEOID'].str.startswith('36087') | gdf1['GEOID'].str.startswith('36079') | gdf1['GEOID'].str.startswith('36103') | gdf1['GEOID'].str.startswith('36059')]
 
 # Filter precincts in gdf2 by gdf['CouName']
-gdf2 = gdf2[gdf2['CouName'].str.startswith('Bergen') | gdf2['CouName'].str.startswith('Hudson') | gdf2['CouName'].str.startswith('Passaic') | gdf2['CouName'].str.startswith('Middlesex') | gdf2['CouName'].str.startswith('Ocean') | gdf2['CouName'].str.startswith('Monmouth') | gdf2['CouName'].str.startswith('Somerset') | gdf2['CouName'].str.startswith('Essex') | gdf2['CouName'].str.startswith('Union') | gdf2['CouName'].str.startswith('Morris') | gdf2['CouName'].str.startswith('Sussex') | gdf2['CouName'].str.startswith('Hunterdon')]
+gdf2 = gdf2[gdf2['CouName'].str.startswith('Hunterdon')]
+
+# Filter precincts in gdf3 by gdf['GEOID']
+gdf3 = gdf3[gdf3['GEOID'].str.startswith('34003') | gdf3['GEOID'].str.startswith('34017') | gdf3['GEOID'].str.startswith('34023') | gdf3['GEOID'].str.startswith('34031') | gdf3['GEOID'].str.startswith('34029') | gdf3['GEOID'].str.startswith('34025') | gdf3['GEOID'].str.startswith('34035') | gdf3['GEOID'].str.startswith('34013') | gdf3['GEOID'].str.startswith('34039') | gdf3['GEOID'].str.startswith('34027') | gdf3['GEOID'].str.startswith('34037')]
 
 # Calculate the Democratic margin in each precinct for gdf2
 gdf2['pct_dem_lead'] = (gdf2['Harris'] - gdf2['Trump']) / gdf2['Total']
 
 # Concatenate the GeoDataFrames
-gdf = gpd.GeoDataFrame(pd.concat([gdf1, gdf2], ignore_index=True))
+gdf = gpd.GeoDataFrame(pd.concat([gdf1, gdf2, gdf3], ignore_index=True))
 gdf = gpd.overlay(gdf, df, how='intersection', keep_geom_type=False)
 
 # Define the bbox for New York-Newark-Jersey City, NY-NJ MSA
